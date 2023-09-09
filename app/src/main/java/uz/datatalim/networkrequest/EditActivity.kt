1d80698ecdbf0744a2131410152c52c04b4004b0
bekhzod234963 <behzod234963@internet.ru>
2023-09-09 19:13:27 +0500

    Network request (Retrofit2)

--- app/src/main/java/uz/datatalim/networkrequest/EditActivity.kt
+++ app/src/main/java/uz/datatalim/networkrequest/EditActivity.kt
@@ -2,10 +2,116 @@ package uz.datatalim.networkrequest
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.ProgressBar
+import android.widget.TextView
+import android.widget.Toast
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+import uz.datatalim.networkrequest.data.remote.ApiClient
+import uz.datatalim.networkrequest.model.Post
 
 class EditActivity : AppCompatActivity() {
+
+    lateinit var progress:ProgressBar
+    lateinit var list:Post
+    lateinit var tvtitle:TextView
+    lateinit var tvbody:TextView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_edit)
+
+        initView()
+
     }
+
+    private fun initView() {
+
+        var id =0
+        id=intent.getIntExtra("id",1)
+        progress=findViewById(R.id.pbProgress)
+        tvtitle=findViewById(R.id.tvTitle)
+        tvbody=findViewById(R.id.tvBody)
+        val btnUpdate:Button=findViewById(R.id.btnUpdate)
+
+        btnUpdate.setOnClickListener {
+
+            val newtitle=tvtitle.text.toString()
+            val newBody=tvbody.text.toString()
+            list.title=newtitle
+            list.body=newBody
+
+            updatePost(list)
+
+        }
+
+        loadPost()
+
+    }
+
+    private fun updatePost(post: Post) {
+
+        showProgress()
+        ApiClient.apiServis.putPost(list.id,post).enqueue(object :Callback<Post>{
+            override fun onResponse(call: Call<Post>, response: Response<Post>) {
+
+                hideProgress()
+                if (response.isSuccessful){
+
+                    Toast.makeText(this@EditActivity, "updated", Toast.LENGTH_SHORT).show()
+
+                }
+
+            }
+
+            override fun onFailure(call: Call<Post>, t: Throwable) {
+
+                hideProgress()
+
+            }
+        })
+
+    }
+
+    fun showProgress(){
+
+        progress.visibility= View.VISIBLE
+
+    }
+    fun hideProgress(){
+
+        progress.visibility= View.GONE
+
+    }
+    private fun loadPost() {
+
+        showProgress()
+        ApiClient.apiServis.getPostById(id = 1).enqueue(object :Callback<Post>{
+            override fun onResponse(call: Call<Post>, response: Response<Post>) {
+
+                if (response.isSuccessful){
+
+                    list=response.body()!!
+                    tvtitle.setText(list.title)
+                    tvbody.setText(list.body)
+
+
+                }
+                hideProgress()
+
+            }
+
+            override fun onFailure(call: Call<Post>, t: Throwable) {
+                hideProgress()
+            }
+
+
+        })
+
+    }
+
+
 }
--- app/src/main/java/uz/datatalim/networkrequest/model/Post.kt
+++ app/src/main/java/uz/datatalim/networkrequest/model/Post.kt
@@ -3,8 +3,8 @@ package uz.datatalim.networkrequest.model
 data class Post(
     val userId:Int,
     val id:Int,
-    val title:String,
-    val body:String
+    var title:String,
+    var body:String
 ) {
     override fun toString(): String {
         return "userId:$userId\nid:$id\ntitle:$title'\nbody:'$body')"
