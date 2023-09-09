0a03b016767f9814bf648a4a33e18aa955ef7454
bekhzod234963 <behzod234963@internet.ru>
2023-09-09 17:33:47 +0500

    Network request (Retrofit2)

--- app/src/main/java/uz/datatalim/networkrequest/data/remote/ApiServis.kt
+++ app/src/main/java/uz/datatalim/networkrequest/data/remote/ApiServis.kt
@@ -1,13 +1,29 @@
 package uz.datatalim.networkrequest.data.remote
 
 import retrofit2.Call
+import retrofit2.http.Body
+import retrofit2.http.DELETE
 import retrofit2.http.GET
 import retrofit2.http.POST
+import retrofit2.http.PUT
+import retrofit2.http.Path
 import uz.datatalim.networkrequest.model.Post
 
 interface ApiServis {
 
     @GET(value = "/posts")
     fun getAll():Call<ArrayList<Post>>
+
+    @GET("/posts/{id}")
+    fun getPostById(@Path("id") id:Int):Call<Post>
+
+    @POST("/posts")
+    fun insertPost(@Body post:Post):Call<Post>
+
+    @PUT("/posts/{id}")
+    fun putPost(@Path("id")id: Int,@Body post: Post):Call<Post>
+
+    @DELETE("/posts/{id}")
+    fun deletePost(@Path("id")id:Int):Call<Post>
 
 }
